37f5a156ae67934b88022d1d4fa1a17877dbbbdb
smallyunet <dev.wangyu@gmail.com>
2022-02-06 19:44:55 +0800

    shell auto commit

---
+++ tcp_server.py
@@ -0,0 +1,18 @@
+import socket
+
+def tcp_server():
+    serversocket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 9999
+    serversocket.bind((host, port))
+    serversocket.listen(5)
+
+    while True:
+        clientsocket, addr = serversocket.accept()
+        print("Got a connection from %s" % str(addr))
+        msg = 'Thank you for connecting' + "\r\n"
+        clientsocket.send(msg.encode('ascii'))
+        clientsocket.close()
+
+tcp_server()
